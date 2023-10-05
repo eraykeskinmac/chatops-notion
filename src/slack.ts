f6e9f77783b7e331a8645363135c57534221ddb7
Eray Keskin <meraykeskin@gmail.com>
2023-10-05 13:50:58 +0300

    fix

--- src/slack.ts
+++ src/slack.ts
@@ -1,11 +1,49 @@
 import type { Handler } from "@netlify/functions";
+import { slackApi, verifySlackRequest } from "./util/slack";
+import { parse } from "querystring";
 
-export const handler: Handler = async () => {
-  // TODO validate the Slack request
+async function handleSlashCommand(payload: SlackSlashCommandPayload) {
+  switch (payload.command) {
+    case "/foodfight":
+      const joke = await fetch("https://icanhazdadjoke.com", {
+        headers: { accept: "text/plain" },
+      });
+      const response = await slackApi("chat.postMessage", {
+        channel: payload.channel_id,
+        text: await joke.text(),
+      });
 
-  // TODO handle slash commands
+      if (!response.ok) {
+        console.log(response);
+      }
+      break;
 
-  // TODO handle interactivity (e.g. context commands, modals)
+    default:
+      return {
+        statusCode: 200,
+        body: `Command ${payload.command} is not recognized`,
+      };
+  }
+  return {
+    statusCode: 200,
+    body: "",
+  };
+}
+
+export const handler: Handler = async (event) => {
+  const valid = verifySlackRequest(event);
+  if (!valid) {
+    console.error("invalid request");
+    return {
+      statusCode: 400,
+      body: "invalid request",
+    };
+  }
+
+  const body = parse(event.body ?? "") as SlackPayload;
+  if (body.command) {
+    return handleSlashCommand(body as SlackSlashCommandPayload);
+  }
 
   return {
     statusCode: 200,
---
+++ src/util/slack.ts
@@ -0,0 +1,39 @@
+import { HandlerEvent } from "@netlify/functions";
+import { createHmac } from "crypto";
+
+export function slackApi(
+  endpoint: SlackApiEndpoint,
+  body: SlackApiRequestBody
+) {
+  return fetch(`https://slack.com/api/${endpoint}`, {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${process.env.SLACK_BOT_OAUTH_TOKEN}`,
+      "Content-Type": "application/json; charset=utf-8",
+    },
+    body: JSON.stringify(body),
+  }).then((res) => res.json());
+}
+
+export function verifySlackRequest(request: HandlerEvent) {
+  const secret = process.env.SLACK_SIGNING_SECRET;
+
+  if (!secret) {
+    throw new Error(
+      "SLACK_SIGNING_SECRET is not set in the environment variables."
+    );
+  }
+
+  const signature = request.headers["x-slack-signature"];
+  const timestamp = Number(request.headers["x-slack-request-timestamp"]);
+  const now = Math.floor(Date.now() / 1000);
+
+  if (Math.abs(now - timestamp) > 300) {
+    return false;
+  }
+
+  const hash = createHmac("sha256", secret)
+    .update(`v0:${timestamp}:${request.body}`)
+    .digest("hex");
+  return `v0=${hash}` === signature;
+}
