74be1ba53780c573386f27abc07ba48bfa15138d
Eray Keskin <meraykeskin@gmail.com>
2023-10-09 18:27:58 +0300

    create a reminder

--- src/reminder.ts
+++ src/reminder.ts
@@ -1,11 +1,29 @@
-import { Handler } from "@netlify/functions";
+import { Handler, schedule } from "@netlify/functions";
 import { getNewItems } from "./util/notion";
-import { slackApi } from "./util/slack";
+import { blocks, slackApi } from "./util/slack";
 
 const postNotionItemsSlack: Handler = async () => {
   const items = await getNewItems();
 
   await slackApi("chat.postMessage", {
-    channel: "C060B8P9CJC"
-  })
+    channel: "C060B8P9CJC",
+    blocks: [
+      blocks.section({
+        text: [
+          "Here are the opinions awaiting judgment:",
+          "",
+          ...items.map(
+            (item) => `- ${item.opinion} (spice level : ${item.spiceLevel})`
+          ),
+          "",
+          `see all items: <https://notion.com/${process.env.NOTION_DATABASE_ID}|in Notion>`,
+        ].join("\n"),
+      }),
+    ],
+  });
+  return {
+    statusCode: 200,
+  };
 };
+
+export const handler = schedule("* * * * *", postNotionItemsSlack);
